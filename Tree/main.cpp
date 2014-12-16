e3fd7b6d1fdd49a2ced1911cde0173e89a62ab34
Marlon Prudente <marlonprudente@users.noreply.github.com>
2014-12-16 10:56:24 -0200

    Update main.cpp

--- Tree/main.cpp
+++ Tree/main.cpp
@@ -1,23 +1,25 @@
 /*
 Elaborar um programa que apresente o seguinte menu:
-1) InserÁ„o em ·rvore bin·ria
-2) RemoÁ„o em ·rvore bin·ria
-3) ApresentaÁ„o da ·rvore
-A ·rvore deve ser apresentada da seguinte forma:
+1) Inser√ß√£o em √°rvore bin√°ria
+2) Remo√ß√£o em √°rvore bin√°ria
+3) Apresenta√ß√£o da √°rvore
+A √°rvore deve ser apresentada da seguinte forma:
 Raiz: 25 FE: 20 FD: 30
-NÛ 20: FE: 10 FD: 23
-NÛ 30: FE: 28 FD: 40
-NÛ 10: FE: 5 FD: 15
-NÛ 23: FE: -1 FD: -1
-NÛ 28: FE: -1 FD: -1
-NÛ 40: FE: -1 FD: -1
-NÛ 5: FE: -1 FD:-1
-NÛ 15: FE: -1 FD: -1
+N√≥ 20: FE: 10 FD: 23
+N√≥ 30: FE: 28 FD: 40
+N√≥ 10: FE: 5 FD: 15
+N√≥ 23: FE: -1 FD: -1
+N√≥ 28: FE: -1 FD: -1
+N√≥ 40: FE: -1 FD: -1
+N√≥ 5: FE: -1 FD:-1
+N√≥ 15: FE: -1 FD: -1
 
 */
 
 #include <iostream>
 #include "include/Tree.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 using namespace std;
 
